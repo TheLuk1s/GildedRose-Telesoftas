aaff0e166c61dbb57109e9aee04743546a7b2022
TheLuk1s <60364075+TheLuk1s@users.noreply.github.com>
2021-05-06 01:26:45 +0300

    Created ItemHandler class

---
+++ php/src/Storage/ItemHandler.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace GildedRose\Storage;
+
+use GildedRose\Item;
+
+use GildedRose\Storage\Items\AgedBrie;
+use GildedRose\Storage\Items\BackstagePasses;
+use GildedRose\Storage\Items\ConjuredManaCake;
+use GildedRose\Storage\Items\SimpleItem;
+use GildedRose\Storage\Items\SulfurasHandOfRagnaros;
+
+class ItemHandler
+{
+    public function __construct(array $qualitySteps = [], bool $ageing = true, ?int $defaultQuality = NULL) {
+    	$this->qualitySteps = $qualitySteps;
+    	$this->ageing = $ageing;
+    	$this->defaultQuality = $defaultQuality;
+    }
+
+    public function getItemData(Item $item) : self
+    {
+		switch ($item->name) {
+            case AgedBrie::NAME:
+            	return new self(AgedBrie::QUALITY_STEPS, AgedBrie::AGEING);
+            case BackstagePasses::NAME:
+                return new self(BackstagePasses::QUALITY_STEPS, BackstagePasses::AGEING);
+            case ConjuredManaCake::NAME:
+                return new self(ConjuredManaCake::QUALITY_STEPS, ConjuredManaCake::AGEING);
+            case SulfurasHandOfRagnaros::NAME:
+                return new self(SulfurasHandOfRagnaros::QUALITY_STEPS, SulfurasHandOfRagnaros::AGEING, SulfurasHandOfRagnaros::DEFAULT_QUALITY);
+            default:
+                return new self(SimpleItem::QUALITY_STEPS, SimpleItem::AGEING);
+        }
+    }
+}
